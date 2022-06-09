d5ff0db49a8abcb992d1e4a9f1e58584314c5137
Bashar Khadra <bashar.work.92@gmail.com>
2022-06-10 01:33:17 +0400

    introducing Api with post and comment models.

---
+++ src/API.ts
@@ -0,0 +1,24 @@
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+export interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+const API_END_POINT = "https://jsonplaceholder.typicode.com";
+
+export async function getAllPosts(): Promise<Post[]> {
+  return fetch(`${API_END_POINT}/posts`).then((resp) => resp.json());
+}
+
+export async function getCommentsByPostId(postId: number): Promise<Comment[]> {
+  return fetch(`${API_END_POINT}/posts/${postId}/comments`).then((resp) =>
+    resp.json()
+  );
+}
